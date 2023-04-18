77d220402548930bbb50a495f81ec7f85aa3bb0e
Aleksandr Sushchenko <sushchenko.aa@phystech.edu>
2023-04-19 01:27:34 +0300

    hide SECRET, update requirements.txt

--- vocabulary/settings.py
+++ vocabulary/settings.py
@@ -12,19 +12,21 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 import os
 from pathlib import Path
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+dotenv_path = os.path.join(BASE_DIR, ".env")
+load_dotenv()
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-qhn0$=uewx^d3q5o5rev5!)inaqnyb#m6#wc2ymo+bwo0=qtu9'
+SECRET_KEY = str(os.getenv("SECRET_KEY"))
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = bool(os.getenv("DEBUG"))
 
 ALLOWED_HOSTS = []
 
